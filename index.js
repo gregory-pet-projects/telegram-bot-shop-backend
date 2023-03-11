d9c3aa21bf27a5255274d315a8f3be0f6f8736eb
GregoryPetrovLn <grishap96123@gmail.com>
2023-03-11 22:09:52 +0800

    fixed index file

--- index.js
+++ index.js
@@ -1,50 +1,17 @@
-const TelegramBot = require("node-telegram-bot-api");
+const express = require("express");
+const cors = require("cors");
+const bot = require("./src/telegram");
+const purchase = require("./src/controllers");
 
-const token = "6031530499:AAEHJr_sLMafic0-fMrRTRwccBbmqr9Q-6M";
-const webAppUrl = "https://voluble-axolotl-47583e.netlify.app";
+const app = express();
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, { polling: true });
+app.use(express.json());
+app.use(cors());
 
-bot.on("message", async (msg) => {
-  const chatId = msg.chat.id;
-  const text = msg.text;
+const PORT = 8000;
 
-  if (text === "/start") {
-    await bot.sendMessage(
-      chatId,
-      "Button will appear bellow, pls fill the form",
-      {
-        reply_markup: {
-          keyboard: [
-            [{ text: "Fill the form", web_app: { url: webAppUrl + "/form" } }],
-          ],
-        },
-      }
-    );
-
-    await bot.sendMessage(chatId, "Visit our website", {
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: "Make an order", web_app: { url: webAppUrl } }],
-        ],
-      },
-    });
-  }
-
-  if (msg?.web_app_data?.data) {
-    try {
-      const data = JSON.parse(msg?.web_app_data?.data);
-      await bot.sendMessage("Thank you");
-      await bot.sendMessage(`Contry:${data.contry}`);
-      await bot.sendMessage(`Street:${data.street}`);
-
-      setTimeout(async () => {
-        await bot.sendMessage(`Full information is availabel on our website`);
-      }, 3000);
-    } catch (e) {}
-  }
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, "Received your message");
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
 });
+
+app.post("/web-data", purchase);
